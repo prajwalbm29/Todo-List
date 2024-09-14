2f7b345c3183ce91b8c989b147d37331721bc9c5
Prajwal B M <bmprajwal43@gmail.com>
2024-09-14 22:46:02 +0530

    v1.0.0

--- frontend/src/Create.jsx
+++ frontend/src/Create.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios'
 
 function Create() {
+    const [task, setTask] = useState()
+    const handleAdd = () => {
+        axios.post('http://localhost:3001/add', {task: task})
+        .then(result => {
+            location.reload()
+        })
+        .catch(err => console.log(err))
+    }
+
     return (
         <div className="create">
-            <input type="text" placeholder="Enter Task" className="ipt" />
-            <button type="button" className="btn">Add</button>
+            <input type="text" placeholder="Enter Task" className="ipt" onChange={(e) => {setTask(e.target.value)}} />
+            <button type="button" className="btn" onClick={handleAdd}>Add</button>
         </div>
     )
 }
--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,22 +1,41 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Create from "./Create";
+import axios from "axios";
 
 function Home() {
     const [todos, setTodos] = useState([])
+    useEffect(() => {
+        axios.get('http://localhost:3001/get')
+        .then(result => {
+            setTodos(result.data);
+        })
+        .catch(err => console.log(err))
+    }, [])
+
+    const handleDelete = (id) => {
+        axios.delete('http://localhost:3001/delete/'+id)
+        .then(result => location.reload())
+        .catch(err => console.log(err))
+    }
+
     return (
         <div className="home">
             <h1>Todo List</h1>
             <Create />
             {
-                todos.length === 0
+                todos.length == 0 
                 ?
-                <div><h2>No Record</h2></div> 
+                    <div><h2>No Records found</h2></div>
                 :
-                todos.map(todo => {
-                    <div>
-                        <h2>{todo}</h2>
+                todos.map((todo, index) => (
+                    <div key={index}>
+                        <div className="homeTodo">
+                            
+                            <h2>{todo.task}</h2>
+                            <button type="button" onClick={() => handleDelete(todo._id)}>DEL</button>
+                        </div>
                     </div>
-                })
+                ))
             }
         </div>
     )
